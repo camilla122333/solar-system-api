8e32e29e47e4478ac4036460ad8593f1cdef85d7
camilla <pasadenastringtheory@gmail.com>
2022-04-25 12:34:19 -0700

    Completed wave 2

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, make_response, abort
 # Define a Planet class with the attributes id, name,
 # and description, and one additional attribute
 # Create a list of Planet instances
@@ -19,16 +19,16 @@ class Planet():
         }
 
 planets = [
-    Planet( 1, "Mercury", ["red", "hot"], False),
-    Planet( 2, "Venus", ["orange", "hot"], False),
-    Planet( 3, "Earth", ["blue", "hot"], True)
+    Planet(1, "Mercury", ["red", "hot"], False),
+    Planet(2, "Venus", ["orange", "hot"], False),
+    Planet(3, "Earth", ["blue", "hot"], True)
 ]
 
-planet_bp = Blueprint("planet_bp", __name__)
-# planet_bp = Blueprint("planet_bp", __name__, url_prefix="/planets")
+# planet_bp = Blueprint("planet_bp", __name__)
+planet_bp = Blueprint("planet_bp", __name__, url_prefix="/planets")
 
 #Get all planets
-@planet_bp.route("/planets", methods=["GET"])
+@planet_bp.route("", methods=["GET"])
 def read_all_planets():
     planets_response = []
 
@@ -37,3 +37,50 @@ def read_all_planets():
 
     return jsonify(planets_response)
 
+'''
+Create the following endpoint(s), with similar functionality
+presented in the Hello Books API:
+
+As a client, I want to send a request...
+...to get one existing planet, so that I can see the id, name, description,
+and other data of the planet.
+... such that trying to get one non-existing planet responds
+with get a 404 response, so that I know the planet resource was not found.
+... such that trying to get one planet with an invalid planet_id responds
+with get a 400 response, so that I know the planet_id was invalid.
+'''
+
+def validate_planet(id):
+    try:
+        id = int(id)
+    except:
+        return abort(make_response({"message": f"planet {id} is invalid"}, 400))
+
+    for planet in planets:
+        if planet.id == id:
+            return planet
+
+    return abort(make_response({"message": f"planet {id} is not found"}, 404))
+
+#Get one planet
+@planet_bp.route("/<id>", methods=["GET"])
+def read_one_planet(id):
+    planet = validate_planet(id)
+
+    return jsonify(planet.to_json(), 200)
+
+
+'''
+def validate_cat(id):
+    try:
+        id = int(id)
+    except:
+        return abort(make_response({"message": f"cat {id} is invalid"}, 400))
+
+    for cat in cats:
+        if cat.id == id:
+            return cat
+
+    return abort(make_response({"message":f"cat {id} not found"}, 404))
+
+'''
